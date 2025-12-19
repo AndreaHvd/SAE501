bf4ea72a6562a6789b512df130c3cb59da55b336
AndreaH <94178978+AndreaHvd@users.noreply.github.com>
2025-12-19 10:36:26 +0100

    Delete listeProduits.php

--- listeProduits.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "fonctions.php" ;
-
-    if (!empty($_POST) && isset($_POST["categorieproduit"])){		
-        $tab = listerProduitsParCategorie($_POST["categorieproduit"]) ;
-        afficheTableau($tab) ;
-    }
-?>
